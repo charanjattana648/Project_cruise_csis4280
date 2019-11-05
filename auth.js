a014cfa4b4a21d1d95bb1862e6cf044667b6d0b2
EC2 Default User <ec2-user@ip-172-31-21-83.ec2.internal>
2019-11-05 08:32:28 +0000

    auth.js is modified

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ function createConnection()
 {
     var connection=mysql.createPool({
         connectionLimit:5,
-        host:'ec2-34-229-86-209.compute-1.amazonaws.com',
+        host:'ec2-54-175-177-16.compute-1.amazonaws.com',
         user:"myuser",
         password:"pass123",
         database:"CruiseUsers"
@@ -14,7 +14,7 @@ function createConnection()
 function signUp(require,response)
 {
     var conn=createConnection();
-    let signUp_query="INSERT INTO Users(email,password) VALUES ?";
+    let signUp_query="INSERT INTO Users(firstName,lastName,email,password) VALUES ?";
     var user=[];
     let data=[];
     data.push(require.body.firstName);
@@ -22,12 +22,14 @@ function signUp(require,response)
     data.push(require.body.email);
     data.push(require.body.psw);
     user.push(data);
+    console.log("Data "+user+"");
     conn.query(signUp_query,[user],function(err,rows,fields)
     {
         if(!err)
         {
             response.send("ok:SignUp succcessfull!!");
         }else{
+            console.log("Error : "+err);
             response.send("Error:SignUp failed Please try with different email");
         }
     });
@@ -36,13 +38,14 @@ function signUp(require,response)
 function login(require,response)
 {
     var conn=createConnection();
-    let login_query="Select * from Users where email="+require.body.email+" and password="+require.body.psw;
+    let login_query="Select * from Users where email='"+require.body.email+"' and password='"+require.body.psw+"'";
     conn.query(login_query,function(err,rows,fields){
         if(!err)
         {
             response.send("ok:Login succcessfull!!");
         }else{
-             response.send("Error:Login failed Please try again");
+            console.log("Error : "+err);
+             response.send("Error:Login failed Please try again ");
         }
         
     })
