71fe41c15190709a6be5ad037eda1b1620db2b03
EC2 Default User <ec2-user@ip-172-31-21-83.ec2.internal>
2019-11-14 04:53:06 +0000

    Modified Mongodb.js

--- Mongodb.js
+++ Mongodb.js
@@ -7,20 +7,250 @@ function getCruiseList(req,res)
     {
         if(err) throw err;
         var dbo=db.db("CruiseDataDB");
-        var collection=dbo.collection("Trips");
+        var collection=dbo.collection("Cruise");
         var query={};
-        collection.find(query,{projection:{CompanyName:1,CruiseName:1,Guests:1,Crew:1,Launched:1,CruiseImage:1,Departs_from:1,_id:0}}).toArray((err,result)=>
+        var cruisename_array=(req.body.cruiseName).split(",");
+        if(req.body.cruiseName!="undefined" && req.body.cruiseName!="")
+        {
+            query={CruiseName:{$in: cruisename_array}};
+        }
+        console.log(query);
+        //  if(req.body.cruiseDestination!="")
+        // {
+        //     query={CompanyName:req.body.cruiseName};
+        // }
+        //  if(req.body.numDays!="")
+        // {
+        //     query={CompanyName:req.body.cruiseName};
+        // }
+        collection.find(query,{projection:{CompanyName:1,CruiseName:1,Guests:1,Crew:1,Launched:1,CruiseImage:1,Departs_from:1,Sails_to:1,_id:0}}).toArray((err,result)=>
         {
              if (err) throw err;
-            
+             console.log("res is :"+result);
+             res.send("CruiseList @:"+JSON.stringify(result));
+             db.close();
+                    
+        })
+    })
+}
+
+
+function getDestinations(req,res)
+{
+    mongoClient.connect(url,{useNewUrlParser:true},function(err,db)
+    {
+        if(err) throw err;
+        var dbo=db.db("CruiseDataDB");
+        var collection=dbo.collection("Packages");
+        //var query={CompanyName:req.body.cruiseName,Sails_to:req.body.cruiseDestination,Num_Days:req.body.numOfDays};
+        var query={};
+        if(req.body.cruiseName!="")
+        {
+          query={CruiseName:req.body.cruiseName};
+        }
+        collection.find(query,{projection:{Sails_to:1,_id:0}}).toArray((err,result)=>
+        {
+             if (err) throw err;
              console.log(result);
-             res.send(JSON.stringify(result));
+             var data= filterArray(result,"Sails_to")
+             console.log("filtered data : "+data.toString());
+             res.send("DestinationsList @:"+data.toString());
              db.close();
                     
         })
         
+    })
+    
+}
 
+function filterArray(result,item)
+{
+      var filtered_res=[];
+             result.forEach(function(itm){
+                 var isfound=false;
+                     filtered_res.forEach((filRes)=>{
+                         if(filRes==itm[item])
+                         {
+                             isfound=true;
+                             return;
+                         }
+                     })
+                     if(isfound==false)
+                     {
+                         filtered_res.push(itm[item]);
+                     }
+                 })
+    return filtered_res
+}
+
+
+function getDays(req,res)
+{
+    mongoClient.connect(url,{useNewUrlParser:true},function(err,db)
+    {
+        if(err) throw err;
+        var dbo=db.db("CruiseDataDB");
+        var collection=dbo.collection("Packages");
+        //var query={CompanyName:req.body.cruiseName,Sails_to:req.body.cruiseDestination,Num_Days:req.body.numOfDays};
+        var query={};
+        if(req.body.cruiseDestination!="")
+        {
+           query={Sails_to:req.body.cruiseDestination};
+        }
+        
+        collection.find(query,{projection:{Num_Days:1,_id:0}}).toArray((err,result)=>
+        {
+             if (err) throw err;
+             console.log(result);
+             var data= filterArray(result,"Num_Days")
+             console.log("filtered data : "+data.toString());
+             res.send("DaysList @:"+data.toString());
+             db.close();
+                    
+        })
     })
 }
 
+function filterNames(req,res)
+{
+    mongoClient.connect(url,{useNewUrlParser:true},function(err, db) {
+        if(err) throw err;
+        var dbo=db.db("CruiseDataDB");
+        var collection=dbo.collection("Packages");
+        var query={};
+        //var query={CompanyName:req.body.cruiseName,Sails_to:req.body.cruiseDestination,Num_Days:req.body.numOfDays};
+        var arr=[];
+        if(req.body.cruiseName!=undefined && req.body.cruiseName!="")
+        {
+            arr["CruiseName"]=req.body.cruiseName;
+        }
+         if(req.body.cruiseDestination!=undefined && req.body.cruiseDestination!="")
+        {
+            arr["Sails_to"]=req.body.cruiseDestination;
+        }
+         if(req.body.numOfDays!=undefined && req.body.numOfDays!="")
+        {
+            arr["Num_Days"]=req.body.numOfDays;
+        }
+        
+        arr.forEach((v,k)=>{
+            console.log(k+" --- "+v);
+        })
+        collection.find(query,{projection:{CruiseName:1,_id:0}}).toArray((err,result)=>{
+            if(err) throw err;
+            console.log("result : "+JSON.stringify(result));
+            var filter_result=filterArray(result,"CruiseName");
+            console.log("res : "+filter_result);
+            res.send("FilteredNames @:"+filter_result);
+            
+        })
+        
+        
+    })
+}
+
+function getCruiseName(req,res)
+{
+    mongoClient.connect(url,{useNewUrlParser:true},function(err,db){
+        if(err) throw err;
+        var dbo=db.db("CruiseDataDB");
+        var collection=dbo.collection("Cruise")
+	    var query={};
+	    collection.find(query,{projection:{CruiseName:1,_id:0}}).toArray((err,result)=>{
+	        if(err) throw err;
+	        console.log(result);
+	        var data= filterArray(result,"CruiseName")
+             console.log("filtered data : "+data.toString());
+             res.send("CruiseNames @:"+data.toString());
+	        db.close();
+	    })
+	  })
+}
+
+function getActivities(req,res)
+{
+    mongoClient.connect(url,{useNewUrlParser:true},function(err, db) {
+       //       CruiseName  Activities Entertainment  Dining
+        if(err) throw err;
+        var dbo=db.db("CruiseDataDB");
+        var collection=dbo.collection("Activity");
+        var query={CruiseName:req.body.cruiseName};
+        collection.find(query,{projection:{Activities:1,_id:0}}).toArray((err,result)=>{
+            if(err) throw err;
+            
+	        var data= filterArray(result,"Activities")
+	        console.log("Entering Activities "+data);
+	        res.send("Activity@:"+data);
+	        
+        })
+      
+    })
+}
+
+function getEntertainment(req,res)
+{
+    mongoClient.connect(url,{useNewUrlParser:true},function(err, db) {
+       //       CruiseName  Activities Entertainment  Dining
+        if(err) throw err;
+        var dbo=db.db("CruiseDataDB");
+        var collection=dbo.collection("Activity");
+        var query={CruiseName:req.body.cruiseName};
+        collection.find(query,{projection:{Entertainment:1,_id:0}}).toArray((err,result)=>{
+            if(err) throw err;
+	        console.log(result);
+	        var data= filterArray(result,"Entertainment")
+	        console.log("Entering Entertainment "+data);
+	        res.send("Entertainment@:"+data);
+	        
+        })
+      
+    })
+}
+
+function getDining(req,res)
+{
+    mongoClient.connect(url,{useNewUrlParser:true},function(err, db) {
+       //       CruiseName  Activities Entertainment  Dining
+        if(err) throw err;
+        var dbo=db.db("CruiseDataDB");
+        var collection=dbo.collection("Activity");
+        var query={CruiseName:req.body.cruiseName};
+        collection.find(query,{projection:{Dining:1,_id:0}}).toArray((err,result)=>{
+            if(err) throw err;
+            
+	        var result_dining=filterArray(result,"Dining");
+	        console.log("Entering Dining: "+result_dining);
+	        res.send("Dining@:"+result_dining);
+        })
+      
+    })
+}
+
+// function getDining(req,res)
+// {
+//     mongoClient.connect(url,{useNewUrlParser:true},function(err, db) {
+//       //       CruiseName  Activities Entertainment  Dining
+//         if(err) throw err;
+//         var dbo=db.db("CruiseDataDB");
+//         var collection=dbo.collection("Activity");
+//         var query={CruiseName:req.body.cruiseName};
+//         collection.find(query,{projection:{Entertainment:1,_id:0}}).toArray((err,result)=>{
+//             if(err) throw err;
+// 	        console.log(result);
+	        
+// 	        res.send(JSON.stringify(result));
+	        
+//         })
+      
+//     })
+// }
+
+
+exports.getDaysList=getDays;
+exports.getDestinationsList=getDestinations;
 exports.getCruiseList=getCruiseList;
+exports.getCruiseNameList=getCruiseName;
+exports.getFilteredNames=filterNames;
+exports.getDiningList=getDining;
+exports.getActivitiesList=getActivities;
+exports.getEntertainmentList=getEntertainment;
